281aa4f3361c1a4fdf2c8f6e1b087eef2c10bd66
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-03 18:16:35 -0300

    feat: add spring project

---
+++ src/test/java/org/example/schoolsystem/SchoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schoolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
