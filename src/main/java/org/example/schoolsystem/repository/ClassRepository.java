576677f819769ccaa635a8524837f75fc68f8d09
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-03 21:06:46 -0300

    feat: add ClassRepository

---
+++ src/main/java/org/example/schoolsystem/repository/ClassRepository.java
@@ -0,0 +1,9 @@
+package org.example.schoolsystem.repository;
+
+import org.example.schoolsystem.model.ClassModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClassRepository extends JpaRepository<ClassModel, UUID> {
+}
