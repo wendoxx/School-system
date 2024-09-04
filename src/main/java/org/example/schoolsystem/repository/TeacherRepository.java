f0dd5075cb01a5b8ed8b4fa77ad1a3debe429b91
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-03 21:06:59 -0300

    feat: add TeacherRepository

---
+++ src/main/java/org/example/schoolsystem/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package org.example.schoolsystem.repository;
+
+import org.example.schoolsystem.model.TeacherModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TeacherRepository extends JpaRepository<TeacherModel, UUID> {
+}
