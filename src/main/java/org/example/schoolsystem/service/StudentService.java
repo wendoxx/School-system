ac17dd4ab7f86af789e0fd5418a6f9246918e3e9
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-04 17:24:34 -0300

    feat add StudentService

---
+++ src/main/java/org/example/schoolsystem/service/StudentService.java
@@ -0,0 +1,35 @@
+package org.example.schoolsystem.service;
+
+import lombok.AllArgsConstructor;
+import org.example.schoolsystem.model.StudentModel;
+import org.example.schoolsystem.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentModel findStudentById(UUID id){
+        return studentRepository.findById(id).get();
+    }
+
+    public StudentModel saveStudent(StudentModel student) {
+        return studentRepository.save(student);
+    }
+
+    public StudentModel deleteStudentById(UUID id) {
+        StudentModel student = studentRepository.findById(id).get();
+        studentRepository.deleteById(id);
+        return student;
+    }
+
+    public List<StudentModel> findAllStudents(){
+        return studentRepository.findAll();
+    }
+
+}
