08de913b57a9adbc1b013d15dc8c627c1df7a22f
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-03 21:05:58 -0300

    feat: add TeacherModel

---
+++ src/main/java/org/example/schoolsystem/model/TeacherModel.java
@@ -0,0 +1,30 @@
+package org.example.schoolsystem.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "teacher")
+@Data
+public class TeacherModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "phone", nullable = false)
+    private String phone;
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @OneToMany(mappedBy = "teacherModel", fetch = FetchType.LAZY)
+    private Set<ClassModel> classModels;
+
+}
