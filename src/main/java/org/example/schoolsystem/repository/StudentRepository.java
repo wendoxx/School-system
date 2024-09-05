166c077938d432289e8e393e27d360ba5e41ad4f
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-05 17:35:19 -0300

    feat: add FindByName in repositories

--- src/main/java/org/example/schoolsystem/repository/StudentRepository.java
+++ src/main/java/org/example/schoolsystem/repository/StudentRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface StudentRepository extends JpaRepository<StudentModel, UUID> {
+    StudentModel findByName(String name);
 }
