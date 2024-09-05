210ad5eb0f6408f93bfbdcbedb06fd3df91bb62c
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-05 18:13:09 -0300

    refactor: StudentController refactored

--- src/main/java/org/example/schoolsystem/controller/StudentController.java
+++ src/main/java/org/example/schoolsystem/controller/StudentController.java
@@ -6,27 +6,39 @@ import org.example.schoolsystem.service.StudentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/api/v1/student")
 @AllArgsConstructor
 public class StudentController {
 
     private final StudentService studentService;
 
-    @GetMapping("/getById")
-    public ResponseEntity<StudentModel> getStudentById(UUID id){
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentModel> getStudentById(@PathVariable UUID id){
         return ResponseEntity.ok(studentService.findStudentById(id));
     }
 
-    @PostMapping("/save")
+    @GetMapping("/byName")
+    public ResponseEntity<StudentModel> getStudentByName(String name){
+        return ResponseEntity.ok(studentService.findByName(name));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<StudentModel>> getAllStudents(){
+        return ResponseEntity.ok(studentService.findAllStudents());
+    }
+
+    @PostMapping
     public ResponseEntity<StudentModel> saveStudent(StudentModel student){
         return ResponseEntity.ok(studentService.saveStudent(student));
     }
 
-    @DeleteMapping("/deleteById")
-    public ResponseEntity<StudentModel> deleteStudentById(UUID id) {
-        return ResponseEntity.ok(studentService.deleteStudentById(id));
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<StudentModel> deleteStudentById(@PathVariable UUID id) {
+        studentService.deleteStudentById(id);
+        return ResponseEntity.noContent().build();
     }
 }
