b2f9d58013b6a98f4a769e8d07fa9d3669b4d9fb
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-12 12:25:54 -0300

    feat: add findBySchoolClass method in StudentRepository

--- src/main/java/org/example/schoolsystem/repository/StudentRepository.java
+++ src/main/java/org/example/schoolsystem/repository/StudentRepository.java
@@ -1,10 +1,15 @@
 package org.example.schoolsystem.repository;
 
+import org.example.schoolsystem.model.ClassModel;
 import org.example.schoolsystem.model.StudentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StudentRepository extends JpaRepository<StudentModel, UUID> {
+    // Find student by name
     StudentModel findByName(String name);
+    // Find student by school class
+    List<StudentModel> findBySchoolClass(ClassModel schoolClass);
 }
