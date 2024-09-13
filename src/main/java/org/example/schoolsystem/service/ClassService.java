51611eb6e94332ba287a18f19436ddd27778f921
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-12 23:45:11 -0300

    feat: add logger

--- src/main/java/org/example/schoolsystem/service/ClassService.java
+++ src/main/java/org/example/schoolsystem/service/ClassService.java
@@ -2,6 +2,7 @@ package org.example.schoolsystem.service;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.Logger;
 import org.example.schoolsystem.dto.ClassDTO;
 import org.example.schoolsystem.model.ClassModel;
 import org.example.schoolsystem.model.StudentModel;
@@ -22,12 +23,15 @@ public class ClassService {
     private final ClassRepository classRepository;
     private final StudentRepository studentRepository;
     private final TeacherRepository teacherRepository;
+    private static final Logger LOGGER = org.apache.logging.log4j.LogManager.getLogger(ClassService.class);
 
     public ClassModel findClassById(UUID id){
+        LOGGER.info("Finding class by id: " + id);
         return classRepository.findById(id).get();
     }
 
     public List<ClassModel> findAllClasses(){
+        LOGGER.info("Finding all classes");
         return classRepository.findAll();
     }
 
@@ -47,12 +51,14 @@ public class ClassService {
         students.forEach(student -> student.setSchoolClass(classModel));
         classModel.setStudents(students);
         classRepository.save(classModel);
+        LOGGER.info("Class saved successfully");
         return classModel;
     }
 
     public ClassModel deleteClassById(UUID id) {
         ClassModel classModel = classRepository.findById(id).get();
         classRepository.deleteById(id);
+        LOGGER.info("Class deleted successfully");
         return classModel;
     }
 
--- src/main/java/org/example/schoolsystem/service/TeacherService.java
+++ src/main/java/org/example/schoolsystem/service/TeacherService.java
@@ -1,6 +1,7 @@
 package org.example.schoolsystem.service;
 
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.Logger;
 import org.example.schoolsystem.dto.TeacherDTO;
 import org.example.schoolsystem.model.TeacherModel;
 import org.example.schoolsystem.repository.TeacherRepository;
@@ -13,16 +14,20 @@ import java.util.UUID;
 @AllArgsConstructor
 public class TeacherService {
     private final TeacherRepository teacherRepository;
+    private static final Logger LOGGER = org.apache.logging.log4j.LogManager.getLogger(TeacherService.class);
 
     public TeacherModel findTeacherById(UUID id){
+        LOGGER.info("Finding teacher by id: " + id);
         return teacherRepository.findById(id).get();
     }
 
     public TeacherModel findByName(String name){
+        LOGGER.info("Finding teacher by name: " + name);
         return teacherRepository.findByName(name);
     }
 
     public List<TeacherModel> findAllTeachers(){
+        LOGGER.info("Finding all teachers");
         return teacherRepository.findAll();
     }
 
@@ -32,12 +37,14 @@ public class TeacherService {
         teacherModel.setAddress(teacher.getAddress());
         teacherModel.setEmail(teacher.getEmail());
         teacherModel.setPhone(teacher.getPhone());
+        LOGGER.info("Teacher saved successfully");
         return teacherRepository.save(teacherModel);
     }
 
     public TeacherModel deleteTeacherById(UUID id) {
         TeacherModel teacher = teacherRepository.findById(id).get();
         teacherRepository.deleteById(id);
+        LOGGER.info("Teacher deleted successfully");
         return teacher;
     }
 
