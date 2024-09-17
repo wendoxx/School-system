6d771ef21a3824dc19dedfb5a870e6ffde94557f
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-16 23:31:07 -0300

    docs: add documentation in TeacherController

--- src/main/java/org/example/schoolsystem/controller/TeacherController.java
+++ src/main/java/org/example/schoolsystem/controller/TeacherController.java
@@ -1,5 +1,8 @@
 package org.example.schoolsystem.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.example.schoolsystem.dto.TeacherDTO;
 import org.example.schoolsystem.model.TeacherModel;
@@ -13,31 +16,70 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v2/teacher")
 @AllArgsConstructor
+@Tag(name = "Teacher controller", description = "This controller is responsible for handling teacher related operations")
+
 public class TeacherController {
 
     private final TeacherService teacherService;
 
+    @Operation(
+            method = "GET",
+            summary = "Get teacher by id",
+            description = "This endpoint retrieves a teacher by its id")
+    @ApiResponse(responseCode = "200", description = "Teacher retrieved successfully")
+    @ApiResponse(responseCode = "404", description = "Teacher not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "400", description = "Bad request")
     @GetMapping("/{id}")
     public ResponseEntity<TeacherModel> getTeacherById(@PathVariable UUID id){
         return ResponseEntity.ok(teacherService.findTeacherById(id));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Get teacher by name",
+            description = "This endpoint retrieves a teacher by its name")
+    @ApiResponse(responseCode = "200", description = "Teacher retrieved successfully")
+    @ApiResponse(responseCode = "404", description = "Teacher not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "400", description = "Bad request")
     @GetMapping("/byName")
     public ResponseEntity<TeacherModel> getTeacherByName(@RequestParam String name){
         return ResponseEntity.ok(teacherService.findByName(name));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Get all teachers",
+            description = "This endpoint retrieves all teachers")
+    @ApiResponse(responseCode = "200", description = "Teachers retrieved successfully")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "400", description = "Bad request")
     @GetMapping("/getAll")
     public ResponseEntity<List<TeacherModel>> getAllTeachers(){
         return ResponseEntity.ok(teacherService.findAllTeachers());
     }
 
+    @Operation(
+            method = "POST",
+            summary = "Save teacher",
+            description = "This endpoint saves a teacher")
+    @ApiResponse(responseCode = "201", description = "Teacher saved successfully")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "400", description = "Bad request")
     @PostMapping
     public ResponseEntity<TeacherModel> saveTeacher(@RequestBody TeacherDTO teacher){
         TeacherModel savedTeacher = teacherService.saveTeacher(teacher);
         return ResponseEntity.status(201).body(savedTeacher);
     }
 
+    @Operation(
+            method = "DELETE",
+            summary = "Delete teacher",
+            description = "This endpoint deletes a teacher")
+    @ApiResponse(responseCode = "204", description = "Teacher deleted successfully")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "400", description = "Bad request")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<TeacherModel> deleteTeacherById(@PathVariable UUID id) {
         teacherService.deleteTeacherById(id);
