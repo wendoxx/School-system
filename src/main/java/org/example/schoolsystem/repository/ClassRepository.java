f0e3ed7beb25e6984b64e2fcfd74780ab3b255fd
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-17 21:04:41 -0300

    feat: add findByClassSubject

--- src/main/java/org/example/schoolsystem/repository/ClassRepository.java
+++ src/main/java/org/example/schoolsystem/repository/ClassRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ClassRepository extends JpaRepository<ClassModel, UUID> {
+    ClassModel findBySubject (String subject);
 }
