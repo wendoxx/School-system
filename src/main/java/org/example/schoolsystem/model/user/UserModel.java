6bdcbfb6f260e0ea0df926de031ee0048268ee51
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-15 21:47:45 -0300

    feat: add UserModel and UserDetails methods

---
+++ src/main/java/org/example/schoolsystem/model/user/UserModel.java
@@ -0,0 +1,70 @@
+package org.example.schoolsystem.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "username" , nullable = false)
+    private String username;
+
+    @Column(name = "password" , nullable = false)
+    private String password;
+
+    @Column(name = "role" , nullable = false)
+    private UserRole role;
+
+    public UserModel(String username, String encryptedPassword, UserRole role) {
+        this.username = username;
+        this.password = encryptedPassword;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role.equals(UserRole.ADMIN)){
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+}
