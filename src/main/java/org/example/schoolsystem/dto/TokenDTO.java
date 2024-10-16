29287903eb9fd8e6e87a002085d95bc69f172650
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-16 14:35:34 -0300

    feat: add TokenDTO

---
+++ src/main/java/org/example/schoolsystem/dto/TokenDTO.java
@@ -0,0 +1,11 @@
+package org.example.schoolsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class TokenDTO {
+    private String token;
+}
